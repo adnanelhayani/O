2f4720864cbe4f380cb58f72fe64501a3056d53e
adnan el hayani <adnanelhayani12@gmail.com>
2025-02-10 11:07:06 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// function playMusic() {
+//     document.getElementById('bg-music').play();
+// }
+
+// function startExperience() {
+//     document.getElementById("content").classList.remove("hidden");
+// }
+
+// function toggleMessage(element) {
+//     let next = element.nextElementSibling;
+//     if (next && next.classList.contains("hidden")) {
+//         next.classList.remove("hidden");
+//     } else {
+//         next.classList.add("hidden");
+//     }
+// }
+
+
+
